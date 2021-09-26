7346e0d200eaa66eee10ead3b8bd638f5a39ee7e
ProjectTPP <tyrykin.yaa@edu.spbstu.ru>
2021-09-26 10:40:22 +0300

    Deleted redundant comments

--- bot_src/bot_config.py
+++ bot_src/bot_config.py
@@ -1,4 +1,6 @@
 from bot_src.tokens import BotTokens
 
-bot_data = {"api_key": BotTokens.YANDEX_API_TOKEN,
-            "api_url": "https://api.rasp.yandex.net/v3.0/"}  # it is necessary to add here both of them
+bot_data = {
+            # "api_key": BotTokens.YANDEX_API_TOKEN,
+            # "api_url": "https://api.rasp.yandex.net/v3.0/"}  # it is necessary to add here both of them
+            "api_url": "http://127.0.0.1:8000/api"}  # it is necessary to add here both of them
